0f02b3186802e7f51bdd82491d5c654394cf376d
neonworker <klausd.lange@live.de>
2020-04-26 01:37:17 +0200

    bcrypt -> bcryptjs

--- routes/authentication/auth.js
+++ routes/authentication/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../../model/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {registerValidation, loginValidation} = require('../../validation');
 
